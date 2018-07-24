588174e8108a7167bea350c2f741d8d4f50758a9
Guy Barros <guy@hashicorp.com>
2018-07-24 13:02:35 +0100

    there is nothing to see here

--- controllers/secret.js
+++ controllers/secret.js
@@ -2,7 +2,7 @@
 var options = {
     apiVersion: 'v1', // default
     endpoint: 'http://127.0.0.1:8080', // default
-    token: 'd21a430e-39dd-0eca-b369-19236e56cb64' // optional client token; can be fetched after valid initialization of the server
+    token: 'd21a430' // optional client token; can be fetched after valid initialization of the server
   };
   
   // get new instance of the client
